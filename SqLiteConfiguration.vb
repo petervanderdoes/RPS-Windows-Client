9fd5c7eb1680cf3bb2378357822b39efef08c230
Peter van der Does <github@avirtualhome.com>
2015-10-23 13:52:07 -0400

    Remove app.config

---
+++ SqLiteConfiguration.vb
@@ -0,0 +1,16 @@
+Imports System.Data.Entity
+Imports System.Data.Entity.Core.Common
+Imports System.Data.SQLite
+Imports System.Data.SQLite.EF6
+
+Public Class SqLiteConfiguration
+    Inherits DbConfiguration
+
+    Public Sub New()
+        SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance)
+        SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance)
+        SetProviderServices("System.Data.SQLite",
+                            DirectCast(SQLiteProviderFactory.Instance.GetService(GetType(DbProviderServices)),
+                                       DbProviderServices))
+    End Sub
+End Class
