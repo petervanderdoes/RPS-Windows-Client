1c4a85a120a5793c507fb6aeb0fc40dbe60c2c62
Peter van der Does <github@avirtualhome.com>
2015-11-16 16:08:42 -0500

    Add checks for failure and success

--- Helpers/Json.vb
+++ Helpers/Json.vb
@@ -13,6 +13,18 @@ Namespace Helpers
                                                           current + error_entry("detail") + Environment.NewLine)
             Return error_messages
         End Function
+        Public Shared Function GetFailureMessage(json As Newtonsoft.Json.Linq.JObject) As String
+
+            Dim error_messages As String = "Server Warning" + Environment.NewLine
+
+            error_messages = error_messages +
+                             Linq.Enumerable.Aggregate(Linq.Enumerable.Cast(Of Object)(json("errors")),
+                                                       "",
+                                                       Function(current, error_entry) _
+                                                          current + error_entry("detail") + Environment.NewLine)
+            Return error_messages
+        End Function
+
 
         Public Shared Function HasErrors(json As Newtonsoft.Json.Linq.JObject) As Boolean
             Return Not IsNothing(json("errors"))
@@ -24,5 +36,18 @@ Namespace Helpers
             End If
             Return False
         End Function
+        Public Shared Function IsFailed(json As Newtonsoft.Json.Linq.JObject) As Boolean
+            If json("status") = "failed" Then
+                Return True
+            End If
+            Return False
+        End Function
+        Public Shared Function IsSuccess(json As Newtonsoft.Json.Linq.JObject) As Boolean
+            If json("status") = "success" Then
+                Return True
+            End If
+            Return False
+        End Function
+
     End Class
 End Namespace
